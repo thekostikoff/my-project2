02f90db869524c053d40c6e0776e9393a961e7f6
thekostikoff <kostikoff_terrik@mail.ru>
2023-03-02 15:54:39 +0300

    task/2:part/3

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,26 @@
         <div class="checkbox__task">
           <input class="checkbox__input" type="checkbox" />
           <label class="checkbox__label" for="checkbox__one">
-            {{ postss.title }}
+            {{ post.title }} {{ post.number }}
           </label>
           <div class="cross"><span></span></div>
         </div>
       </div>
+      <div class="btn">
+        <p>Add a new task</p>
+      </div>
+      <div class="todo__info">
+        <div class="oneofthird">1/3 left</div>
+        <div class="task__info">
+          <p>All</p>
+          <p>Active</p>
+          <p>Completed</p>
+        </div>
+      </div>
     </div>
-    <div class="img"><img v-bind:src="image" />{{ image }}</div>
+    <div class="img">
+      <img :src="require('../image/list.png')" />
+    </div>
   </div>
 </template>
 
@@ -22,9 +35,9 @@ export default {
     return {
       image: "../image/list.png",
       postss: [
-        { id: 1, title: "Task 1" },
-        { id: 2, title: "Task 2" },
-        { id: 3, title: "Task 3" },
+        { id: 1, title: "Task", number: 1 },
+        { id: 2, title: "Task", number: 2 },
+        { id: 3, title: "Task", number: 3 },
       ],
     };
   },
@@ -49,9 +62,10 @@ body {
 }
 .general {
   width: 540px;
-  min-height: 437px;
-  border: 2px solid #ffca93;
+  border: 1px solid #ffca93;
   box-shadow: 0px 8px 35px 5px #fe9262;
+  position: relative;
+  z-index: 2;
 }
 .todo__list {
   width: 540px;
@@ -61,13 +75,11 @@ body {
   font-style: normal;
   font-weight: 600;
   font-size: 20px;
-  line-height: 28px;
   padding: 11px 228px;
 }
 .post {
   background: #fff4e9;
-  min-height: 337px;
-  padding: 30px 30px 25px 30px;
+  padding: 15px 30px 10px 30px;
 }
 .checkbox__input {
   width: 24px;
@@ -92,7 +104,11 @@ body {
 }
 .img {
   width: 400px;
-  height: 512px;
+  min-height: 512px;
+  position: relative;
+  top: -415px;
+  left: 280px;
+  z-index: 1;
 }
 .cross {
   display: block;
@@ -101,7 +117,7 @@ body {
   height: 24px;
   border: 1.5px solid #ffca93;
   border-radius: 2px;
-  margin-left: 330px;
+  margin-left: 320px;
 }
 .cross span {
   position: absolute;
@@ -133,5 +149,51 @@ body {
 .cross:before,
 .cross:after:hover {
   background-color: #fc8f1a;
+}
+.btn {
+  width: 480px;
+  height: 50px;
+  background: #ffecd8;
+  border: 1.5px dashed #ffca93;
+  border-radius: 10px;
+  box-sizing: border-box;
+  margin: 0px auto;
+  padding: 11px 167px;
+}
+.btn p {
+  font-family: "Inter";
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 28px;
+  color: #ffca93;
+}
+.todo__info {
+  display: flex;
+  flex-direction: row;
+  align-items: self-start;
+  justify-content: center;
+  gap: 200px;
+  width: 540px;
+  height: 50px;
+  background: #ffca93;
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 20px;
+  color: #c9955d;
+  line-height: 28px;
+  padding: 11px 228px;
+}
+.task__info {
+  display: flex;
+  gap: 25px;
+}
+.task__info p:hover {
+  border: 1px solid #c9955d;
+  border-radius: 10px;
+}
+.oneofthird {
+  padding-left: 10px;
+  min-width: 100px;
 }
 </style>
