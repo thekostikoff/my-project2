107c3c845a1ea71184ef0cedb454268b3d741fbe
thekostikoff <kostikoff_terrik@mail.ru>
2023-03-14 16:19:25 +0300

    task/2:part/4

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,23 @@
       <div class="post" id="post" v-for="post in postss" v-bind:key="post.id">
         <div class="checkbox__task">
           <div class="task">
-            <input class="checkbox__input" type="checkbox" />
-            <label class="checkbox__label" for="checkbox__one">
+            <input class="checkbox__input" type="checkbox" id="checkbox" />
+            <label class="checkbox__label" for="checkbox">
               {{ post.title }} {{ post.number }}
             </label>
           </div>
-          <div class="cross"><span></span></div>
+          <button class="cross"><span></span></button>
         </div>
-        <button class="btn">
-          <p>Add a new task</p>
-        </button>
       </div>
+      <button class="btn">
+        <p>Add a new task</p>
+      </button>
       <div class="todo__info">
         <div class="oneofthird">1/3 left</div>
         <div class="task__info">
-          <p>All</p>
-          <p>Active</p>
-          <p>Completed</p>
+          <button class="task__btn">All</button>
+          <button class="task__btn">Active</button>
+          <button class="task__btn">Completed</button>
         </div>
       </div>
     </div>
@@ -51,8 +51,9 @@ export default {
 }
 body {
   position: relative;
-  width: 1178px;
-  min-height: 640px;
+  max-width: 100vw;
+  width: 100%;
+  min-height: 100vh;
   background: linear-gradient(104.11deg, #ff7e5f 14.52%, #feb567 87.26%);
 }
 .todoapp {
@@ -66,23 +67,18 @@ body {
   box-shadow: 0px 8px 35px 5px #fe9262;
   position: relative;
   z-index: 2;
+  background: #fff4e9;
 }
 .todo__list {
   background: #ffca93;
   font-family: "Open Sans";
-  font-style: normal;
   font-weight: 600;
-  font-size: 20px;
   padding: 11px 228px;
+  color: #7f4b13;
 }
 .post {
   background: #fff4e9;
-  padding: 15px 30px 0px 30px;
-}
-.checkbox__input {
-  width: 24px;
-  height: 24px;
-  margin-left: 20px;
+  padding: 15px 0px 0px 30px;
 }
 .checkbox__task {
   max-width: 480px;
@@ -90,14 +86,59 @@ body {
   border-radius: 10px;
   display: flex;
   flex-direction: row;
-  padding: 13px 0;
+  padding: 0 0;
+}
+.task {
+  position: relative;
+  margin: 13px 15px;
+}
+.checkbox__input {
+  -webkit-appearance: none;
+  appearance: none;
+  position: absolute;
 }
 .checkbox__label {
-  font-family: "Inter";
-  font-style: normal;
+  padding-left: 40px;
   font-size: 20px;
-  padding-left: 20px;
+  font-family: "Inter";
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 28px;
   color: #7f4b13;
+  cursor: pointer;
+}
+.checkbox__label:before {
+  content: "";
+  display: block;
+  width: 24px;
+  height: 24px;
+  border: 1.5px solid #feb567;
+  border-radius: 5px;
+  background: #ffdfbe;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+}
+.checkbox__label:after {
+  content: "";
+  display: block;
+  width: 24px;
+  height: 24px;
+  background: url("../image/V.svg") no-repeat;
+  background-size: 13.71px 9.43px;
+  opacity: 0;
+  position: absolute;
+  top: 7px;
+  left: 6px;
+  z-index: 2;
+}
+.checkbox__input:checked + .checkbox__label:before {
+  background: #feb567;
+  border-radius: 5px;
+}
+.checkbox__input:checked + .checkbox__label:after {
+  opacity: 1;
 }
 .cross {
   display: block;
@@ -106,7 +147,8 @@ body {
   height: 24px;
   border: 1.5px solid #ffca93;
   border-radius: 2px;
-  margin-left: 320px;
+  margin-top: 13px;
+  margin-left: 300px;
 }
 .cross span {
   position: absolute;
@@ -131,6 +173,7 @@ body {
 }
 .cross:hover {
   border: 1.5px solid #fc8f1a;
+  cursor: pointer;
 }
 .cross:hover:before,
 .cross:hover:after,
@@ -144,8 +187,9 @@ body {
   border: 1.5px dashed #ffca93;
   border-radius: 10px;
   box-sizing: border-box;
-  margin: 15px auto;
+  margin: 15px 30px;
   padding: 11px 176px;
+  cursor: pointer;
 }
 .btn p {
   font-family: "Inter";
@@ -174,11 +218,21 @@ body {
   display: flex;
   gap: 25px;
 }
-.task__info p:hover {
+.task__btn {
+  background: #ffca93;
+  font-family: "Open Sans";
+  font-weight: 600;
+  font-size: 19px;
+  color: #c9955d;
+  border: 0;
+}
+.task__info button:hover {
   border: 1px solid #c9955d;
   border-radius: 10px;
+  cursor: pointer;
 }
 .oneofthird {
+  font-family: "Open Sans";
   padding-left: 10px;
   min-width: 100px;
 }
